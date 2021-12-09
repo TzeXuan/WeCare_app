b2a89db5d83ec9d7538ae29e2b54478f8a47fc57
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2021-12-09 21:03:56 +0800

    Add package SignUp

---
+++ app/src/main/java/com/example/wecare_app/SignUp/SignUpPage1.kt
@@ -0,0 +1,11 @@
+package com.example.wecare_app.SignUp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignUpPage1 : Fragment() {
+
+}
