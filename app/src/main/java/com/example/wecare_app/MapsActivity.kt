c19dd3cb6cdf4cc86b90e38e586ff3e0626e40b1
simzeki123 <simzeki1583@gmail.com>
2021-12-12 21:41:16 +0800

    Added: myClusterManagerRenderer.kt

--- app/src/main/java/com/example/wecare_app/MapsActivity.kt
+++ app/src/main/java/com/example/wecare_app/MapsActivity.kt
@@ -436,28 +436,3 @@ private fun intit(){
 
 }
 
-class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapter {
-
-    var mContext = context
-    var mWindow = (context as Activity).layoutInflater.inflate(R.layout.custom_info_window, null)
-
-    private fun rendowWindowText(marker: Marker, view: View){
-
-        val tvTitle = view.findViewById<TextView>(R.id.title)
-        val tvSnippet = view.findViewById<TextView>(R.id.snippet)
-
-        tvTitle.text = marker.title
-        tvSnippet.text = marker.snippet
-
-    }
-
-    override fun getInfoContents(marker: Marker): View {
-        rendowWindowText(marker, mWindow)
-        return mWindow
-    }
-
-    override fun getInfoWindow(marker: Marker): View? {
-        rendowWindowText(marker, mWindow)
-        return mWindow
-    }
-}
