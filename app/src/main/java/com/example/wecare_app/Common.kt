db9deaf757a9bd83b866a1992c487ac2186aae81
TzeXuan <70616530+TzeXuan@users.noreply.github.com>
2021-12-13 17:05:14 +0800

    Add news layout

---
+++ app/src/main/java/com/example/wecare_app/Common.kt
@@ -0,0 +1,4 @@
+package com.example.wecare_app
+
+class Common {
+}
---
+++ app/src/main/java/com/example/wecare_app/ListNews.kt
@@ -0,0 +1,11 @@
+package com.example.wecare_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_news)
+    }
+}
